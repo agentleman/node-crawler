55c61eaa99db7e60b1bc4f8ed0148c1c43c52e0f
agentleman <675740051@qq.com>
2021-02-22 18:05:20 +0800

    cheerio 节点遍历

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
-const request = require('superagent');  //发送请求模块
-const cheerio = require('cheerio');     //转对象模块
-const Koa = require('koa');             //搭建服务模块
+const cheerio = require("cheerio"); //转对象模块
+const Koa = require("koa"); //搭建服务模块
+const Router = require("koa-router");
+const charset = require("superagent-charset");
+const superagent = charset(require("superagent")); //发送请求模块 一个轻量的ajax API
 
 const App = new Koa();
+const router = new Router();
 
-const url = 'https://www.lagou.com/zhaopin/webqianduan/?labelWords=label'; 
+// const url = "https://www.baidu.com";
+// router.get("/", (ctx, next) => {
+//   ctx.body = " 程序创建成功！";
+// });
 
-App.use(async ctx =>{
-  const arr = [];   //存放爬取到的数据
-  const data = await new Promise(resolve =>{
-    request
-      .post(url)  //爬取数据请求的地址
-      .end((err, res)=>{
-        const data = res.text;  //请求到的html文档
-        const $ = cheerio.load(data); //html转对象
-        //去分析原网页的dom结构 li的class为.con_list_item
-        $('.con_list_item').each((i,v)=>{
-          const $v = $(v);
-          const obj = { //爬取class=position_link的a标签的href
-            src: $v.find('a.position_link').prop("href"),
-            zhili: $v.find('.li_b_l').text().trim(),
-            money: $v.find('.money').text().trim(),
-            name: $v.find('.company_name a').prop("href"),
-            industry: $v.find('.industry').text().trim(),
-          }
-          arr.push(obj);
-        })
-        resolve(arr)
-      })
-  })
-  ctx.body = arr; //将爬取的数据返回给前端
-  console.log(arr)
+router.get("/", (ctx, next) => {
+  url = "https://s.weibo.com/top/summary?cate=realtimehot"; //target地址
+  superagent
+    .get(url)
+    .charset("utf-8")
+    .buffer(true)
+    .end((err, data) => {
+      if (err) {
+        console.log("页面不存在", err);
+      }
+      // console.log("=======================" + JSON.stringify(data.text));
+      let html = data.text,
+        $ = cheerio.load(html, {
+          decodeEntities: false,
+          ignoreWhitespace: false,
+          xmlMode: false,
+          lowerCaseTags: false,
+        }), //用cheerio解析页面数据
+        obj = {};
+      arr = [];
+      // cheerio的使用类似jquery的操作
+      $("table tbody").each((index, element) => {
+        let $element = $(element);
+        console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'+$element)
+        // console.log(JSON.stringify($element.find("a").text()));
+        // $element
+        //   .find(".td-02")
+        //   .next()
+        //   .find("a")
+        //   .addClass("link")
+        //   .attr("class", "link")
+        //   .text("");
+        // arr.push({
+        //   title: $element.find("a.blue14b").text(),
+        //   image: $element.find("#bright img").attr("src"),
+        //   summary: $element.find("#tctitle").next().text(),
+        //   is_cgiia:
+        //     $element.find("#tctitle font").attr("color") === "green" ? 1 : 0,
+        // });
+        arr.push({
+          title: $element.find("a").text(),
+          url: $element.find("a").attr("href"),
+        });
+      });
+    });
+  ctx.body = arr;
+  // ctx.body = data;
+});
 
-})
+App.use(router.routes()).use(router.allowedMethods());
 
-
-
-
-App.listen("8080", () => {
-  console.log('爬虫服务已启动！！！');
-  console.log(cheerio)
+App.listen("31001", () => {
+  console.log("服务已启动！！！");
+  console.log(cheerio);
 });
