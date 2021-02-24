8d35657157ee7b703c0af565abf442d64fe690db
agentleman <675740051@qq.com>
2021-02-24 14:02:21 +0800

    热点写入

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const Koa = require("koa"); //搭建服务模块
 const Router = require("koa-router");
 const charset = require("superagent-charset");
 const superagent = charset(require("superagent")); //发送请求模块 一个轻量的ajax API
+const fs = require("fs");
+const path = require("path");
 
 const App = new Koa();
 const router = new Router();
-// const arr = []
+const arr = [];
 
-router.get("/", (ctx, next) => {
+router.get("/", async (ctx, next) => {
   let url = "https://s.weibo.com/top/summary?cate=realtimehot"; //target地址
   // let url = "https://segmentfault.com/a/1190000016655289"; //target地址
-  
-  // let arr = [];
-
   superagent
     .get(url)
     .charset("utf-8")
@@ -28,20 +27,48 @@ router.get("/", (ctx, next) => {
           ignoreWhitespace: false,
           xmlMode: false,
           lowerCaseTags: false,
-        }),
-        //用cheerio解析页面数据
-        obj = {};
-       arr = [];
+        });
+      //用cheerio解析页面数据
+      this.arr = [];
       // cheerio的使用类似jquery的操作
+      let that = this;
       $("table tbody tr").each((index, element) => {
         let $element = $(element);
-        arr.push({
+        that.arr.push({
           title: $element.find(".td-02").find("a").text(),
           url: $element.find(".td-02").find("a").attr("href"),
         });
       });
+
+      fs.writeFile(
+        path.join(
+          path.resolve(__dirname, "../"),
+          "node-crawler/hot/output.txt"
+        ),
+        "### 今日热点",
+        (err) => {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
+      for (let i = 0; i < that.arr.length; i++) {
+        fs.appendFile(
+          "output.md",
+          i + "[" + that.arr[i].title + "]" + "[" + that.arr[i].url + "]</br>",
+          (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            //完成！
+          }
+        );
+      }
     });
-  ctx.body = this.arr;
+
+  // await next()
+  ctx.response.body = this.arr;
 });
 
 App.use(router.routes()).use(router.allowedMethods());
